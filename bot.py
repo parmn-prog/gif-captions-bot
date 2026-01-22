6b1d804cc228cbb5fe934edad6a792a7b50850ad
parmn <172978878+parmn-prog@users.noreply.github.com>
2026-01-22 17:48:51 +0530

    Automod functionality

--- bot.py
+++ bot.py
@@ -3,7 +3,9 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import re
+import random
 
+paths = ["lb.png", "lang.jpg"]
 
 BANNED_WORDS: str
 
@@ -20,6 +22,7 @@ load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.auto_moderation = True
 
 client = discord.Client(intents=intents)
 
@@ -28,16 +31,22 @@ async def on_ready():
     print(f'We have logged in as {client.user}')
 
 @client.event
+async def on_automod_action(execution):
+    print("executed")
+    await execution.channel.send(file=discord.File(random.choice(paths)))
+
+@client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
     for line in BANNED_WORDS.splitlines():
-        if line in message.content.lower():
+        if (line + " " in message.content.lower() and line[0] == message.content[0])\
+            or (" " + line in message.content.lower() and line[-1] == message.content[-1]) or\
+        " " + line + " " in message.content.lower() or line == message.content.lower():
             print("BAD WORD")
-            await message.channel.send(content=f"{message.author.mention}" ,file=discord.File("lb.png"))
+            await message.channel.send(content=f"{message.author.mention}",file=discord.File(random.choice(paths)))
             break
-    
 
 
 token = os.getenv("BOT_TOKEN")
